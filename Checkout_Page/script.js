01d7a4b0e20f8ecda7cd244455383b79aa524c0b
haliltuzcu <007canberk@gmail.com>
2023-01-21 22:28:04 +0300

    jskismenbitti

--- Checkout_Page/script.js
+++ Checkout_Page/script.js
@@ -1,11 +1,14 @@
-
 const conteinerRight = document.querySelector(".conteiner_right");
 const product12 = document.querySelectorAll(".product");
 const tax = 1.18;
 const shipping = "15.00";
-document.querySelector(`.product_currentPrice0`).innerText=25.98
-document.querySelector(`.product_currentPrice1`).innerText=48.9
-document.querySelector(`.product_currentPrice2`).innerText=75.2
+let subtotalPrice = document.querySelector(".subtotal_price");
+let taxPrice = document.querySelector(".tax_price");
+let shippingPrice = document.querySelector(".shipping_price");
+let totalPrice = document.querySelector(".total_price");
+document.querySelector(`.product_currentPrice0`).innerText = 25;
+document.querySelector(`.product_currentPrice1`).innerText = 48;
+document.querySelector(`.product_currentPrice2`).innerText = 75;
 let products = JSON.parse(localStorage.getItem("products")) || [];
 
 window.addEventListener("load", () => {
@@ -32,13 +35,19 @@ conteinerRight.addEventListener("click", (e) => {
     }
   }
   if (e.target.className == "btn_remove") {
+    let a = Number(
+      e.target.parentElement.nextElementSibling.children[0].innerText
+    );
+    console.log(a);
+    let l =subtotalPrice.innerText - a
+    subtotalPrice.innerText = l
     e.target.closest(".product").remove();
+    
   }
-  e.target.closest(`.product_currentPrice`).innerText=0;
 });
 
 let productTotal = () => {
-  let y = 0
+  let y = 0;
   for (i = 0; i < product12.length; i++) {
     let price1 = Number(
       document.querySelector(`.product_currentPrice${i}`).innerText
@@ -47,26 +56,27 @@ let productTotal = () => {
       document.querySelector(`.urunMiktari${i}`).innerText
     );
     let ProductTotal = document.querySelector(`.product_totalPrice${i}`);
-    let x = +(price1 * quantity1).toFixed(2);
+    let x = +(price1 * quantity1)
     ProductTotal.innerText = x;
-    y+=x
+    y += x;
   }
-  let subtotalPrice = document.querySelector(".subtotal_price")
-  subtotalPrice.innerText =+y.toFixed(2)
-  let z = +((y*tax)-y).toFixed(2)
-  let taxPrice = document.querySelector(".tax_price")
-  taxPrice.innerText = z
-  let shippingPrice = document.querySelector(".shipping_price")
-  shippingPrice.innerText = shipping
-let totalPrice = document.querySelector(".total_price")
-let k = +(z+y+15).toFixed(2)
-totalPrice.innerText =k
 
+  subtotalPrice.innerText = +y
 
+  let z = +(y * tax - y)
+  taxPrice.innerText = z.toFixed(2);
+  
+  shippingPrice.innerText = shipping;
+  let k = +(z  + y + 15.00)
+  totalPrice.innerText = k;
+ k.parseFloat().toFixed(2)
 };
 
+
+
 // const product = {
-//   suptotal:,
-//   completed : false, 
+//   suptotal:
+//   completed : false,
 //   text:,}
-// localStorage.setItem("products",JSON.stringify(products))
+
+localStorage.setItem("products", JSON.stringify(products));
