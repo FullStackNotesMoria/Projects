9d9b76b1820c86285e64eb70cc3c422e4b47d658
erhanatalan <erhanatalan2013@gmail.com>
2023-01-29 13:31:51 +0300

    son tt

--- weather_app/script.js
+++ weather_app/script.js
@@ -4,10 +4,7 @@ const select = document.querySelector(".form-select")
 const go = document.querySelector("#go")
 //console.log(select.value);
 
-const getResult = (cityName) =>{
-    let query = `${URL}weather?q=${cityName}&appid=${apiKey}&units=metric&lang=tr`
-    console.log(query);
-}
+
 go.addEventListener("click", (e)=>{
     if(select.value){
         getWeather();
@@ -17,7 +14,7 @@ go.addEventListener("click", (e)=>{
 
 const getWeather = async () =>{
     const URL = "https://api.openweathermap.org/data/2.5/"
-    const apiKey= "7b9b390baa88161879be04f030424915"
+    const apiKey= "2a593cca2507cce399c056cfcd13534b"
     const cityName = option[select.value].innerText;
     let getURL = `${URL}weather?q=${cityName}&appid=${apiKey}&units=metric&lang=tr`
 
@@ -28,17 +25,28 @@ const getWeather = async () =>{
             throw new Error("Something went wrong")
         }
         const data = await res.json()
+
+        document.querySelector(".durum").innerHTML=`
+        <div class="row justify-content-center mt-3">
+            <div class="card" style="width: 18rem;">
+                <img src="./img/${data.weather[0].icon}.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${data.name}</h5>
+                <p class="card-text">${data.weather[0].description.toUpperCase()}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${new Date().toLocaleTimeString()}</li>
+                <li class="list-group-item">Sıcaklık: ${Math.round(data.main.temp)}°C</li>
+                <li class="list-group-item">Hissedilen Sıcaklık: ${Math.round(data.main.feels_like)}°C</li>
+                <li class="list-group-item">En Yüksek Sıcaklık: ${Math.round(data.main.temp_max)}°C</li>
+                <li class="list-group-item">En Düşük Sıcaklık: ${Math.round(data.main.temp_min)}°C</li>
+                <li class="list-group-item">Nem: ${data.main.humidity}%</li>
+                <li class="list-group-item">Hava Kapalılık Oranı: ${data.clouds.all}%</li>
+                <li class="list-group-item">Rüzgar Hızı: ${data.wind.speed}km/s</li>
+            </ul>
+        </div>
+        `
         console.log(data);
-        // hava durumu aciklama
-        console.log(data.weather[0].description);
-        // nem
-        console.log(data.main.humidity);
-        // suanda sicaklik
-        console.log(data.main.temp);
-        // mın sicaklik
-        console.log(data.main.temp_min);
-        //max sicaklik
-        console.log(data.main.temp_max);
 
     }catch(error){
         console.log(error);
