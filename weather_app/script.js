e9a2cc1b4d23cdc319815f00a6fb1411a97e52e4
erhanatalan <erhanatalan2013@gmail.com>
2023-01-29 23:33:15 +0300

    game over

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,9 +1,17 @@
 const option = Array.from(document.querySelectorAll("option"))
+const options = document.querySelector("option")
 //console.log(option);
 const select = document.querySelector(".form-select")
 const go = document.querySelector("#go")
 //console.log(select.value);
 
+
+select.addEventListener("change", ()=>{
+    // if(select.value){
+    //     getWeather();
+    // }
+    go.click()
+})
 
 go.addEventListener("click", (e)=>{
     if(select.value){
@@ -27,23 +35,24 @@ const getWeather = async () =>{
         const data = await res.json()
 
         document.querySelector(".durum").innerHTML=`
-        <div class="row justify-content-center mt-3">
+        <div class="row justify-content-center mt-3 ">
             <div class="card" style="width: 18rem;">
                 <img src="./img/${data.weather[0].icon}.png" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">${data.name}</h5>
-                <p class="card-text">${data.weather[0].description.toUpperCase()}</p>
+                    <h5 class="card-title">${data.name}</h5>
+                    <p class="card-text">${data.weather[0].description.toUpperCase()}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${new Date().toLocaleTimeString()}</li>
+                    <li class="list-group-item">Sıcaklık: ${Math.round(data.main.temp)}°C</li>
+                    <li class="list-group-item">Hissedilen Sıcaklık: ${Math.round(data.main.feels_like)}°C</li>
+                    <li class="list-group-item">En Yüksek Sıcaklık: ${Math.round(data.main.temp_max)}°C</li>
+                    <li class="list-group-item">En Düşük Sıcaklık: ${Math.round(data.main.temp_min)}°C</li>
+                    <li class="list-group-item">Nem: ${data.main.humidity}%</li>
+                    <li class="list-group-item">Hava Kapalılık Oranı: ${data.clouds.all}%</li>
+                    <li class="list-group-item">Rüzgar Hızı: ${data.wind.speed}km/s</li>
+                </ul>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">${new Date().toLocaleTimeString()}</li>
-                <li class="list-group-item">Sıcaklık: ${Math.round(data.main.temp)}°C</li>
-                <li class="list-group-item">Hissedilen Sıcaklık: ${Math.round(data.main.feels_like)}°C</li>
-                <li class="list-group-item">En Yüksek Sıcaklık: ${Math.round(data.main.temp_max)}°C</li>
-                <li class="list-group-item">En Düşük Sıcaklık: ${Math.round(data.main.temp_min)}°C</li>
-                <li class="list-group-item">Nem: ${data.main.humidity}%</li>
-                <li class="list-group-item">Hava Kapalılık Oranı: ${data.clouds.all}%</li>
-                <li class="list-group-item">Rüzgar Hızı: ${data.wind.speed}km/s</li>
-            </ul>
         </div>
         `
         console.log(data);
