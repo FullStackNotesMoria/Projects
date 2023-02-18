5ea783b83660ffd11cb811dea26c0c207176a52e
erhanatalan <erhanatalan2013@gmail.com>
2023-02-18 18:46:11 +0300

    styled-companent

--- tour_place/src/components/Card/Card.jsx
+++ tour_place/src/components/Card/Card.jsx
@@ -1,12 +1,16 @@
 
 
 const Card = ({title,desc,img}) => {
+  const kay ={
+    
+  }
+
   return (
     <div className="card text-bg-dark" style={{width: '32rem'}}>
         <div className="card-header">{title}</div>
         <div className="card-body">
           <img src={img} alt="img" style={{width:"100%", height:"360px"}}/>
-          {/* <p>{desc}</p> */}
+          <p className="kay">{desc}</p>
         </div>
       </div>
   )
