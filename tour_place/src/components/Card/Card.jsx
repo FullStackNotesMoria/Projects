52e11fdb57e3e56b102b7fbbf9563678b6ac284e
erhanatalan <erhanatalan2013@gmail.com>
2023-02-18 16:59:05 +0300

    img

--- tour_place/src/components/Card/Card.jsx
+++ tour_place/src/components/Card/Card.jsx
@@ -3,10 +3,10 @@
 const Card = ({id,title,desc,img}) => {
   return (
     <div className="card text-bg-dark mb-3" style={{maxWidth: '18rem'}}>
-        <div className="card-header">Header</div>
+        <div className="card-header">{title}</div>
         <div className="card-body">
-          <h5 className="card-title">Dark card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <img src={img} alt="img"/>
+          <p>{desc}</p>
         </div>
       </div>
   )
