664ac088443a49d9ddc15445265a8453ceb9a992
erhanatalan <erhanatalan2013@gmail.com>
2023-02-24 01:38:42 +0300

    xsx

--- Checkout_Page_React/src/components/Main/Main.jsx
+++ Checkout_Page_React/src/components/Main/Main.jsx
@@ -2,27 +2,38 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "../Card/Card";
+import styled from "styled-components";
 
+const Containerx = styled.div`
+.card-container{
+  .row{
+    .col{
+      .card{
+        margin-left: 25px;
+        margin-bottom: 15px;
+        height:600px;
 
-const Main = ({data}) => {
+      }
+    }
+  }
+}
+`;
+const Main = ({ data }) => {
   return (
-    <>
-      <Container className="card-container rounded-4 my-4 p-3">
-        <Row className="g-5 justify-content-center">
-          {
-            data.map((x,i)=>{
-              return(
-                x.Stok > 0 ? 
-                <Col md={6} lg={4} xl={3} key={i} >
-                  <Card data={x} /> 
-                </Col> : null
-              );
-            })
-          }
+    <Containerx>
+      <Container className="card-container rounded-4 my-2 p-1">
+        <Row className="justify-content-center row">
+          {data.map((x, i) => {
+            return x.Stok > 0 ? (
+              <Col md={6} lg={4} xl={3} key={i} className="col">
+                <Card data={x} className="card" />
+              </Col>
+            ) : null;
+          })}
         </Row>
       </Container>
-    </>
-  )
-}
+    </Containerx>
+  );
+};
 
 export default Main;
