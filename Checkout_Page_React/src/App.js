1b05dafb5b4430afb47e9b98ad7bd6d58f4edab5
erhanatalan <erhanatalan2013@gmail.com>
2023-02-24 00:41:17 +0300

    s

--- Checkout_Page_React/src/App.js
+++ Checkout_Page_React/src/App.js
@@ -4,16 +4,18 @@ import data from "./Data/xmltojson.json"
 
 
 function App() {
+  const fetch = require('node-fetch');
+  const convert = require('xml-js');
+  let dataAsJson = {};
 
-  const postTutorial = async (newTutorial) => {
-    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
-    try {
-      await axios.post(BASE_URL, newTutorial)
-    } catch (error) {
-      console.log(error)
-    }
-    getTutorials()
-  }
+  fetch('http://www.bilgisayarim.com.tr/xml?clientcode=24284&username=admin&password=Ankara2020')
+    .then(response => response.text())
+    .then(str => {
+        dataAsJson = JSON.parse(convert.xml2json(str))
+    })
+    .then(() => {
+        console.log(`xx :${dataAsJson}`);
+    });
 
   // console.log(data);
   return (
