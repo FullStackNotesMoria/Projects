51d9a748c4adaa55ce036bffee1382314d94b3ac
erhanatalan <erhanatalan2013@gmail.com>
2023-02-24 00:32:42 +0300

    x

--- Checkout_Page_React/src/App.js
+++ Checkout_Page_React/src/App.js
@@ -4,6 +4,17 @@ import data from "./Data/xmltojson.json"
 
 
 function App() {
+
+  const postTutorial = async (newTutorial) => {
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+    try {
+      await axios.post(BASE_URL, newTutorial)
+    } catch (error) {
+      console.log(error)
+    }
+    getTutorials()
+  }
+
   // console.log(data);
   return (
     <>
