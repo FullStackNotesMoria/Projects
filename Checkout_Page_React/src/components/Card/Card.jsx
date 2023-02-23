f6439d665d8bccb2fd50aa1a4d18c62b3758a326
erhanatalan <erhanatalan2013@gmail.com>
2023-02-23 19:56:06 +0300

    des

--- Checkout_Page_React/src/components/Card/Card.jsx
+++ Checkout_Page_React/src/components/Card/Card.jsx
@@ -2,13 +2,8 @@ import { FaStar, FaCartPlus } from "react-icons/fa";
 import styled from "styled-components";
 
 const DivStyles = styled.div`
-  .row {
-    .col {
-      .card {
-        width: 350px;
-      }
-    }
-  }
+  margin: 10px;
+  width: 350px;
 `;
 
 const Card = ({ data }) => {
@@ -43,50 +38,45 @@ const Card = ({ data }) => {
     UrunKodu,
   } = data;
   return (
-    <DivStyles className="container d-flex justify-content-center mt-50 mb-50">
-      <div className="row">
-        <div className="col-md-4 mt-2 col">
-          <div className="card">
-            <div className="card-body">
-              <div className="card-img-actions">
-                <img
-                  src={GorselBuyuk}
-                  className="card-img img-fluid"
-                  width={96}
-                  height={350}
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="card-body bg-light text-center">
-              <div className="mb-2">
-                <h6 className="font-weight-semibold mb-2">
-                  <a href="#" className="text-default mb-2" data-abc="true">
-                    {UrunAciklama}
-                  </a>
-                </h6>
-                <a href="#" className="text-muted" data-abc="true">
-                  {Marka}
-                </a>
-              </div>
-              <h3 className="mb-0 font-weight-semibold">
-                {Fiyat}
-                {DovizTuru}
-              </h3>
-              <div>
-                <FaStar size={22} color=" #d35400" />
-                <FaStar size={22} color=" #d35400" />
-                <FaStar size={22} color=" #d35400" />
-                <FaStar size={22} color=" #d35400" />
-                <FaStar size={22} color=" #d35400" />
-              </div>
-              <div className="text-muted mb-3">34 reviews</div>
-              <button type="button" className="btn bg-cart">
-                <FaCartPlus className="me-2" size={22} color="white" /> Add to
-                cart
-              </button>
-            </div>
+    <DivStyles>
+      <div className="card">
+        <div className="card-body">
+          <div className="card-img-actions">
+            <img
+              src={GorselBuyuk}
+              className="card-img img-fluid"
+              width={96}
+              height={350}
+              alt=""
+            />
           </div>
+        </div>
+        <div className="card-body bg-light text-center">
+          <div className="mb-2">
+            <h6 className="font-weight-semibold mb-2">
+              <a href="#" className="text-default mb-2" data-abc="true">
+                {UrunAciklama}
+              </a>
+            </h6>
+            <a href="#" className="text-muted" data-abc="true">
+              {Marka}
+            </a>
+          </div>
+          <h3 className="mb-0 font-weight-semibold">
+            {Fiyat}
+            {DovizTuru}
+          </h3>
+          <div>
+            <FaStar size={22} color=" #d35400" />
+            <FaStar size={22} color=" #d35400" />
+            <FaStar size={22} color=" #d35400" />
+            <FaStar size={22} color=" #d35400" />
+            <FaStar size={22} color=" #d35400" />
+          </div>
+          <div className="text-muted mb-3">34 reviews</div>
+          <button type="button" className="btn bg-cart">
+            <FaCartPlus className="me-2" size={22} color="white" /> Add to cart
+          </button>
         </div>
       </div>
     </DivStyles>
--- Checkout_Page_React/src/components/Main/Main.jsx
+++ Checkout_Page_React/src/components/Main/Main.jsx
@@ -1,20 +1,28 @@
-import Card from "../Card/Card"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "../Card/Card";
 
 
 const Main = ({data}) => {
-  
-  // data.map((x)=>console.log(x.UrunID))
   return (
-    <div>
-      {
-        data.map((x,i)=>{
-          return(
-            x.Stok > 0 ?
-            <Card key={i} data={x}/> : null)
-        })
-      }
-    </div>
+    <>
+      <Container className="card-container rounded-4 my-4 p-3">
+        <Row className="g-5 justify-content-center">
+          {
+            data.map((x,i)=>{
+              return(
+                x.Stok > 0 ? 
+                <Col md={6} lg={4} xl={3} key={i} >
+                  <Card data={x} /> 
+                </Col> : null
+              );
+            })
+          }
+        </Row>
+      </Container>
+    </>
   )
 }
 
-export default Main
+export default Main;
